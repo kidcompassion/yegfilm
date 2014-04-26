4da31fc340dcbfd43917886ab966f59d41bf0edf
kidcompassion <sallypoulsen@gmail.com>
2014-04-25 22:39:04 -0600

    Adds in devise and configures routes and links.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Yegfilm::Application.routes.draw do
+  devise_for :users
   get "profiles/index"
   resources :profiles
-  root 'profiles#index'
+  root to: 'profiles#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
