43e3432b1218ad0fc2949068ebfb5e8894cd6e97
Sally Poulsen <sallypoulsen@gmail.com>
2014-05-03 16:29:53 -0600

    Production update.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,10 +27,10 @@ Yegfilm::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Precompile all assets
-  config.assets.precompile = ['*.js', '*.css', '*.css.erb']
+  config.assets.precompile = ['*.js', '*.css', '*.css.erb', '*.jpg']
 
   # Generate digests for assets URLs.
   config.assets.digest = true
